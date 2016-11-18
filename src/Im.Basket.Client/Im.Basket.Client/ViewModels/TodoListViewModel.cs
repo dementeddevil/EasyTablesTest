3e467b7958be78af18b33b643f2fac15bc4ecec5
AdrianLewis <adrian.lewis@immediate.co.uk>
2016-11-18 10:36:10 +0000

    Added view and view model objects.

---
+++ src/Im.Basket.Client/Im.Basket.Client/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Im.Basket.Client.Entities;
+
+namespace Im.Basket.Client.ViewModels
+{
+    public class TodoListViewModel : TableViewModel<TodoItem>
+    {
+        public TodoListViewModel(IAzureDataTableClient client) : base(client)
+        {
+        }
+    }
+}
