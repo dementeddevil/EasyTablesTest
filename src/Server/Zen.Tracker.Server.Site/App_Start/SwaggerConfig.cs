b669bc707d2f1d2a3b7c84b7f62297516de417d1
DementedDevil <zen_global@hotmail.com>
2017-01-01 22:24:45 +0000

    Tweaked swagger config

--- src/Server/Zen.Tracker.Server.Site/App_Start/SwaggerConfig.cs
+++ src/Server/Zen.Tracker.Server.Site/App_Start/SwaggerConfig.cs
@@ -13,7 +13,20 @@ namespace Zen.Tracker.Server.Site
                     c =>
                     {
                         // Single API version information
-                        c.SingleApiVersion("v1", "Zen Tracker");
+                        c.SingleApiVersion("v1", "Zen Tracker")
+                            .Description("API for interacting with the Zen Tracker todo repository")
+                            .Contact(
+                                cc => cc
+                                .Name("Developer Support")
+                                .Email("api@zendesignsoftware.com")
+                                .Url("http://www.zendesignsoftware.com/"))
+                            .License(
+                                li => li
+                                .Name("Limited License")
+                                .Url("http://www.zendesignsoftware.com/tracker/api/license"));
+
+                        // Enable use of HTTP andd HTTPS schemes
+                        c.Schemes(new[] { "http", "https" });
 
                         // Set this flag to omit descriptions for any actions decorated with the Obsolete attribute
                         c.IgnoreObsoleteActions();
@@ -22,10 +35,7 @@ namespace Zen.Tracker.Server.Site
                         // Incorporate annotated Controllers and API Types with Xml comments 
                         c.IncludeXmlComments(GetXmlCommentsPath());
                     })
-                .EnableSwaggerUi(
-                    c =>
-                    {
-                    });
+                .EnableSwaggerUi();
         }
 
         private static string GetXmlCommentsPath()
